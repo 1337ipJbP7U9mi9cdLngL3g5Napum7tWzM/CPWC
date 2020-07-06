b09f27e00034ea32219ad1c321e8711e14552a5e
DooM <1337ipJbP7U9mi9cdLngL3g5Napum7tWzM@protonmail.com>
2020-07-06 15:50:44 -0500

    Add session structure

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :contacts,  only: [:index, :new, :create, :destroy]
   resources :posts
+  resources :sessions, only: [:new, :create]
 
   get 'contacts/destroy_all', to: 'contacts#destroy_all'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
