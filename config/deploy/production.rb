bb7994b78931ee57e46e61f52a347c1967783d83
DooM <1337ipJbP7U9mi9cdLngL3g5Napum7tWzM@protonmail.com>
2020-10-06 11:21:55 -0500

    capistrano tweaks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server Rails.application.credentials.linode[:ip], user: Rails.application.credentials.linode[:user], roles: %w{app db web}
 
 
 # role-based syntax
