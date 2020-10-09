7232b8da0d3e89e046515cee23b8fb5bfd3cb4ee
DooM <1337ipJbP7U9mi9cdLngL3g5Napum7tWzM@protonmail.com>
2020-10-08 22:49:33 -0500

    app spreadsheet export input and fix extra space infront of address

--- app/javascript/controllers/csv_generator_controller.js
+++ app/javascript/controllers/csv_generator_controller.js
@@ -19,17 +19,29 @@ export default class extends Controller {
     const totalAddresses = cpwc_addresses.totalAddresses()
     const totalCrypto = cpwc_addresses.totalCryptoAmount()
     const totalFiat = parseFloat(cpwc_addresses.totalFiatAmount())
+    const csvNameInput = document.getElementById('csv_generator_input')
 
+    console.log(csvNameInput)
 
+    let csvName = ''
+    if(csvNameInput.value !== '') {
+      console.log('hi 1')
+      csvName = csvNameInput.value + '.csv'
+    } else {
+      console.log('hi 2')
+      csvName = 'CryptocurrenciesChecker.csv'
+    }
+
+    console.log(csvName)
 
     csv = `Addresses: ${totalAddresses}, ${cryptoName}: ${totalCrypto},${fiatName}: ${totalFiat}`
 
     cpwc_addresses.addresses.map((row) => {
-      csv += ` \r\n ${row.address}, ${row.crypto_amount}, ${row.fiat_amount}`
+      csv += `\r\n${row.address}, ${row.crypto_amount}, ${row.fiat_amount}`
     })
 
     var blob = new Blob([csv], {type: "text/plain;charset=utf-8"});
-    FileSaver.saveAs(blob, "bye.csv");
+    FileSaver.saveAs(blob, csvName);
   }
 
 }
